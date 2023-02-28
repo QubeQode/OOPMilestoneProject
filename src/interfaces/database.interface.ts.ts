0bcf8f8286fd0ff37cac100cb3cb51ac428a4c95
jerry <jerryfan71@gmail.com>
2023-02-27 21:45:01 -0800

    finishing tailwind

--- src/interfaces/database.interface.ts.ts
+++ src/interfaces/database.interface.ts.ts
@@ -1,6 +1,6 @@
 import IUser from "./user.interface";
 
-// ⭐️ Feel free to change this interface to your liking
+// ⭐️ Feel free to change this interface to your liking. change this to users to post users and comments. fisst step look at jeddit and the fakedb stuff.
 export default interface IDatabase {
   users: IUser[];
 }
