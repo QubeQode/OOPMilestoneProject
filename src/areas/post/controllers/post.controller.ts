89d89ee27c698f74ae6d649eaada834f7c208e0c
Jun <junability@gmail.com>
2023-03-09 14:17:41 -0800

    routesPseudoCode

--- src/areas/post/controllers/post.controller.ts
+++ src/areas/post/controllers/post.controller.ts
@@ -21,18 +21,47 @@ class PostController implements IController {
 
   // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary posts object
   private getAllPosts = (_: Request, res: Response) => {
+    /*
+    1. find current user via session
+    2. find current user via followers
+    3. randomly select posts from followers
+    4. select latest posts from current user
+    5. sort posts by created_at value
+
+    render pfp
+    render created_at value
+    render username
+    render msg
+    render comment# and repost#
+    */
     res.render("post/views/posts", { posts });
   };
 
   // 🚀 This method should use your postService and pull from your actual fakeDB, not the temporary post object
   private getPostById = async (request: Request, res: Response, next: NextFunction) => {
+    /*
+    1. search posts table with id
+    2. return post data
+
+    render pfp
+    render username
+    render created_at
+    render msg
+    render comments and reposts
+    */
     res.render("post/views/post", { post });
   };
 
   // 🚀 These post methods needs to be implemented by you
-  private createComment = async (req: Request, res: Response, next: NextFunction) => {};
-  private createPost = async (req: Request, res: Response, next: NextFunction) => {};
-  private deletePost = async (req: Request, res: Response, next: NextFunction) => {};
+  private createComment = async (req: Request, res: Response, next: NextFunction) => {
+    
+  };
+  private createPost = async (req: Request, res: Response, next: NextFunction) => {
+
+  };
+  private deletePost = async (req: Request, res: Response, next: NextFunction) => {
+
+  };
 }
 
 export default PostController;
