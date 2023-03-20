f7768b5faf234520263aeacc45a35f12e4ac5004
jerry <jerryfan71@gmail.com>
2023-03-20 13:29:11 -0700

    updated passport configuration

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,71 +3,64 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-import passport from 'passport';
-import { passportStrategy} from '/../interfaces/passport.interface';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { IAuthenticationService } from '../services/IAuthentication.service'
-
-
+import passport from "passport";
+import { passportStrategy } from "/../interfaces/passport.interface";
+import { Strategy as LocalStrategy } from "passport-local";
+import { IAuthenticationService } from "../services/IAuthentication.service";
 
 export default class PassportConfig {
-    authenticationService: any;
+  private _authenticationService: IAuthenticationService;
 
-    constructor(strategies: passportStrategy[], authenticationService: IAuthenticationService) {
-        this.addStrategies(strategies);
-    }
+  constructor(strategies: passportStrategy[], authenticationService: IAuthenticationService) {
+    this.addStrategies(strategies);
+    this._authenticationService = authenticationService;
+  }
 
+  private addStrategies(strategies: passportStrategy[]): void {
+    strategies.forEach((passportStrategy: passportStrategy) => {
+      passport.use(passportStrategy.name, passportStrategy.strategy);
+    });
+  }
 
-    private addStrategies(strategies: passportStrategy[]): void {
-        strategies.forEach((passportStrategy: passportStrategy) => {
-            passport.use(passportStrategy.name, passportStrategy.strategy)
-        })
-    }
+  private localStrategy(): void {
+    passport.use(
+      new LocalStrategy(
+        {
+          usernameField: "email",
+          passwordField: "password",
+        },
+        (email, password, done) => {
+          this._authenticationService
+            .getUserByEmailAndPassword(email, password)
+            .then((user) => {
+              if (!user) {
+                return done(null, false, { message: "Incorrect email." });
+              }
+              if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password." });
+              }
+              return done(null, user);
+            })
+            .catch((err) => done(err));
+        }
+      )
+    );
+  }
 
-    
+  private serializeUser(): void {
+    passport.serializeUser((user: any, done: (err: any, id: number) => void) => {
+      done(null, user.id);
+    });
+  }
 
-    private localStrategy (): void {
-        passport.use(new LocalStrategy(
-            {
-                usernameField: 'email',
-                passwordField: 'password'
-            },
-            (email, password, done) => {
-                this.authenticationService.getUserByEmailAndPassword(email, password)
-                .then((user) => {
-                    if (!user) {
-                        return done(null, false, { message: 'Incorrect email.' });
-                    }
-                    if (user.password !== password) {
-                        return done(null, false, { message: 'Incorrect password.' });
-                    }
-                    return done(null, user);
-                })
-                .catch((err) => done(err));
-            }
-        ));
-    }
-
-    private serializeUser(): void {
-        passport.serializeUser((user: any, 
-            done: (err: any, id: number) => void) => {
-            done(null, user.id);
-        })
-    }
-
-    private deserializeUser(): void {
-         passport.deserializeUser(function (id: number,
-             done: (error: any, user: Express.User | false | null) => void) {
-                let user = getUserById(id);
-                if (user) {
-                    done (null, user)
-                } else {
-                    done({ message: 'User not found'}, null)
-                }
-         });
-    }
-}
-function getUserById(id: number) {
-    
-    throw new Error('Function not implemented.');
+  private deserializeUser(): void {
+    passport.deserializeUser(function (id: number, done: (error: any, user: Express.User | false | null) => void) {
+      let user = this._authenticationService.getUserById(id);
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    });
+  }
 }
